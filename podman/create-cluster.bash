a329300cd5b0d8487a8ec0a277ce82590de60417
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-18 16:59:50 +0900

    nginx

--- podman/create-cluster.bash
+++ podman/create-cluster.bash
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
-KIND_EXPERIMENTAL_PROVIDER=podman kind create cluster --config=kind-config.yaml
+kind create cluster --config=kind-config.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 podman save localhost/rails -o rails.tgz
-KIND_EXPERIMENTAL_PROVIDER=podman kind load image-archive rails.tgz
+kind load image-archive rails.tgz
+podman save localhost/nginx -o nginx.tgz
+kind load image-archive nginx.tgz
 kubectl apply -k .
