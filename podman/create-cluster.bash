ee8019ddfb64424d371054d788f0898974c34896
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-06-27 12:35:04 +0900

    add migrate and delete rails pv

--- podman/create-cluster.bash
+++ podman/create-cluster.bash
@@ -20,3 +20,18 @@ rm ./nginx.tgz
 
 # apply all
 kubectl apply -k .
+
+# rails job
+for i in {1..10}; do
+  echo "Waiting for postgresql..${i}/10"
+  sleep 5;
+  STATUS=$(kubectl get pods --selector app=postgresql -o json | jq -r ".items[].status.containerStatuses[].ready");
+  echo ${STATUS}
+  if [ ${STATUS} = "true" ]; then
+    break;
+  fi
+done
+
+kubectl apply -f ./rails/jobs/job-db-create.yaml
+sleep 5
+kubectl apply -f ./rails/jobs/job-db-migrate.yaml
