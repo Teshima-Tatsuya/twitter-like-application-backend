3d3fa6b1d68afd4fd3f18db7d228e47adbff995d
Tatsuya Teshima <terima666tatsuya@gmail.com>
2024-07-17 17:03:45 +0900

    fix migrate

--- db/migrate/20240712010320_create_users.rb
+++ db/migrate/20240712010320_create_users.rb
@@ -2,10 +2,11 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name, null: false, limit: 100
-      t.string :email, null: false, limit: 319
+      t.string :email, null: false, limit: 254
       t.string :password_digest, null: false, limit: 128
 
       t.timestamps
     end
+    add_index :users, :email, unique: true
   end
 end
--- db/migrate/20240716011056_create_follows.rb
+++ db/migrate/20240716011056_create_follows.rb
@@ -1,10 +1,12 @@
 class CreateFollows < ActiveRecord::Migration[7.1]
   def change
     create_table :follows do |t|
-      t.integer :following_user_id
-      t.integer :follower_user_id
+      t.references :following_user, null: false, foreign_key: { to_table: :users }
+      t.references :follower_user, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
+
+    add_index :follows, [:following_user_id, :follower_user_id], unique: true
   end
 end
